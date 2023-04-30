824d41f3f72cc1d8174471987bf4126105e0a8a2
Sergo331 <proha331@gmail.com>
2023-04-30 18:22:19 +0300

    задача 2-4
    задача 2-4

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задача_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberMin = 1;
+            int numberMax = 101;
+            int randomNumber;
+            int numberSum = 0;
+            int nemberMultiple1 = 3;
+            int nemberMultiple2 = 5;
+            Random random = new Random();
+
+            randomNumber = random.Next(numberMin, numberMax);
+            Console.WriteLine(randomNumber);
+
+            for (int i = 0; i < randomNumber; i++)
+            {
+                if (i % nemberMultiple1 == 0 || i % nemberMultiple2 == 0)
+                {
+                    numberSum += i;
+                }
+            }
+
+            Console.WriteLine($"Сумма всех чисел, которые кратны {nemberMultiple1} или {nemberMultiple2}:");
+            Console.WriteLine(numberSum);
+
+
+        }
+    }
+}
